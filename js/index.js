33469873cb765cec9a2abd9f133ff57e0b834f22
Arian Cordoba <arian.gasolero@gmail.com>
2022-10-19 01:51:45 -0300

    Mejorando la interaccion con el usuario, se agregó un formulario para una mejor visualizacion

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function calcular() {
+	let monto = 0;
+	let descuento = 0;
+	let imp_desc = 0;
+	let total = 0;
+
+	monto = document.getElementById("monto").value;
+	descuento = document.getElementById("descuento").value;
+
+	imp_desc = (monto * descuento) / 100;
+
+	document.getElementById("importe_descontado").value = imp_desc;
+
+	total = monto - imp_desc;
+
+	document.getElementById("total").value = total;
+}
